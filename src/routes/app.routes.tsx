3ac3e009189ea37aa497814bd494fb48620603ca
Victornfb <victornfb@outlook.com>
2024-01-27 18:02:36 -0300

    style: ident app routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -34,12 +34,12 @@ export function AppRoutes() {
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
         tabBarStyle: {
-        backgroundColor: colors.gray[600],
-        borderTopWidth: 0,
-        height: Platform.OS === "android" ? 'auto' : 96,
-        paddingBottom: sizes[10],
-        paddingTop: sizes[6]
-      }
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? 'auto' : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6]
+        }
       }}
     >
       <Screen 
