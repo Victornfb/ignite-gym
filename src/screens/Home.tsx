99291021d316641ed11181d81f861db1b3009ac3
Victornfb <victornfb@outlook.com>
2024-01-29 22:10:03 -0300

    feat: add group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,35 @@
-import { VStack } from 'native-base';
+import { FlatList, VStack } from 'native-base';
+import { useState } from 'react';
 
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('Costas')
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
